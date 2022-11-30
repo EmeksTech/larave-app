69803b09bfdf438901e2794a68738755f7f6102d
EmeksTech <115981145+EmeksTech@users.noreply.github.com>
2022-11-30 11:23:18 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+
+this is my contact page
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+
+this is my index page
+@endsection
