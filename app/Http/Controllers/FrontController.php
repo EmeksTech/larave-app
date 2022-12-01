611bf0931fc2e1b1e4398b51e707f07119a6e242
EmeksTech <115981145+EmeksTech@users.noreply.github.com>
2022-12-01 11:34:55 +0100

    second commit

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index(){
-      return view('index');
+        $posts = Post::all();
+      return view('index',['posts' => $posts]);
     }
     public function about(){
         return view('about');
     }
     public function contact(){
-        
+
         return view('contact');
 
     }
